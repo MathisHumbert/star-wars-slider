8494431556ae5a16ee8217a02f39d7382914ccce
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-21 11:15:05 +0100

    project is done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react';
-import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
+import { useState } from 'react';
 import styled from 'styled-components';
+import Bottom from './components/Bottom';
 import Top from './components/Top';
 import data from './data';
-let tempData = [1, 2, 3, 4, 5];
 
 function App() {
   const [index, setIndex] = useState(1);
-
-  useEffect(() => {
-    const lastIndex = data.length;
-    if (index < 1) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(1);
-    }
-  }, [index]);
 
   return (
     <Wrapper>
@@ -26,26 +15,7 @@ function App() {
             return <Top key={item.id} {...item} index={index} />;
           })}
         </div>
-        <div className='bottom'>
-          <div className='left'>
-            <button onClick={() => setIndex(index - 1)}>
-              <BsArrowLeft />
-            </button>
-            <button onClick={() => setIndex(index + 1)}>
-              <BsArrowRight />
-            </button>
-          </div>
-          <div className='right'>
-            {tempData.map((item) => {
-              return (
-                <div key={item} className='circle-line-container'>
-                  <div className='circle'></div>
-                  <div className='line'></div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <Bottom setIndex={setIndex} index={index} />
       </div>
     </Wrapper>
   );
@@ -65,63 +35,6 @@ const Wrapper = styled.div`
     height: calc(100vh - 451px);
     position: relative;
   }
-  /* bottom */
-  .bottom {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 100px;
-  }
-
-  .bottom .left {
-    display: flex;
-    gap: 45px;
-  }
-
-  button {
-    background: none;
-    width: fit-content;
-    font-size: 35px;
-    line-height: 0;
-    border: 0;
-    color: rgba(130, 123, 104, 1);
-  }
-
-  .bottom .right {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-  }
-
-  .circle-line-container {
-    position: relative;
-    width: 100%;
-
-    &:last-child .line {
-      display: none;
-    }
-
-    &:last-child {
-      width: fit-content;
-    }
-  }
-
-  .circle {
-    width: 32px;
-    height: 32px;
-    background: rgba(255, 243, 138, 1);
-    border-radius: 50%;
-  }
-
-  .line {
-    position: absolute;
-    height: 1px;
-    background: rgba(130, 123, 104, 1);
-    width: 100%;
-    top: 50%;
-    left: 32px;
-    transform: translateY(-50%);
-  }
 `;
+
 export default App;
--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,22 +1,32 @@
 import styled from 'styled-components';
 import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
-const Bottom = () => {
+let data = [1, 2, 3, 4, 5];
+
+const Bottom = ({ setIndex, index }) => {
   return (
     <Wrapper>
       <div className='left'>
-        <button>
+        <button
+          onClick={() => setIndex((index) => index - 1)}
+          className={index === 1 ? null : 'active'}
+          disabled={index === 1}
+        >
           <BsArrowLeft />
         </button>
-        <button>
+        <button
+          onClick={() => setIndex((index) => index + 1)}
+          className={index === 3 ? null : 'active'}
+          disabled={index === 3}
+        >
           <BsArrowRight />
         </button>
       </div>
       <div className='right'>
-        {data.map((item, index) => {
+        {data.map((item) => {
           return (
-            <div>
-              <div className='circle'></div>
-              <div className='line'></div>
+            <div key={item} className='circle-line-container'>
+              <div className={item <= index ? 'active circle' : 'circle'}></div>
+              <div className={item <= index - 1 ? 'active line' : 'line'}></div>
             </div>
           );
         })}
@@ -25,5 +35,93 @@ const Bottom = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 100px;
+
+  .left {
+    display: flex;
+    gap: 45px;
+  }
+
+  button {
+    background: none;
+    width: fit-content;
+    font-size: 35px;
+    line-height: 0;
+    border: 0;
+    color: rgba(130, 123, 104, 1);
+    cursor: pointer;
+
+    &.active {
+      color: rgba(255, 243, 138, 1);
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+
+  .right {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .circle-line-container {
+    position: relative;
+    width: 100%;
+
+    &:last-child .line {
+      display: none;
+    }
+
+    &:last-child {
+      width: fit-content;
+    }
+  }
+
+  .circle {
+    width: 32px;
+    height: 32px;
+    background: rgba(130, 123, 104, 1);
+    border-radius: 50%;
+    transition: background 0.5s linear;
+
+    &.active {
+      background: rgba(255, 243, 138, 1);
+    }
+  }
+
+  .line {
+    position: absolute;
+    height: 1px;
+    background: rgba(130, 123, 104, 1);
+    width: 100%;
+    top: 50%;
+    left: 32px;
+    transform: translateY(-50%);
+
+    &:after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: rgba(255, 243, 138, 1);
+      width: 0%;
+      top: 50%;
+      left: 0;
+      transform: translateY(-50%);
+      transition: width 0.5s linear;
+    }
+
+    &.active {
+      &:after {
+        width: 100%;
+      }
+    }
+  }
+`;
 export default Bottom;
